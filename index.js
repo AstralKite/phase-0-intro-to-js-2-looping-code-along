cad7979b89874762f0b147c771698d9e6a4a3f13
Fausto Ramirez <ramirezmedia@gmail.com>
2022-12-02 06:58:31 -0500

    commit tree 422f9689d710fac713650fd73bb552f559201f0f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(myarray, message){
+    let newArray = [];
+
+    for(let i = 0; i < myarray.length; i++){
+        newArray[i] = `Thank you, ${myarray[i]}, for the wonderful ${message} gift!`;
+    }
+    return newArray
+}
+
+
+function countDown(start){
+    for(let i = start; i >= 0; i--)
+    {
+        console.log(i);
+    }
+}
